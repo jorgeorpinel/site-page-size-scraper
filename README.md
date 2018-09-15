a96ccf8d97dd7ce2b5d9b38c90f746f0c9958b4f
Jorge Orpinel <jorge@orpinel.com>
2018-09-15 08:17:25 -0500

    Completes 10 top site list; Improves README and in-line doc; Improves stdout, adds timeouts, and handles exceptions.

--- README.md
+++ README.md
@@ -4,14 +4,17 @@ Author: Jorge Orpinel Perez @jorgeorpinel
 # Website Page Size Scraper
 
 Python script that uses Selenium and Headless Chrome to determine the average page size among a list of websites.
+This will include [transferSize](https://www.w3.org/TR/resource-timing-2/#dom-performanceresourcetiming-transfersize) AND any other content loaded dynamically to display the home page of each site.
 
 ## Installation
 
 This tool was developed and ran with Python 3.6.5 on macOS 10.13
 
+> Further versions should continue to work.
+
 ### External dependencies
 
-- [Chrome](https://www.google.com/chrome/) 68 installed
+- [Chrome](https://www.google.com/chrome/) 68+ installed
 - [ChromeDriver](https://sites.google.com/a/chromium.org/chromedriver) in the OS path - chromedriver 2.42 used
 
 ### Required Python package
@@ -25,18 +28,24 @@ virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
 ```
-> Virtualenv installs [pip](https://pip.pypa.io/)
+> Virtualenv installs [pip](https://pip.pypa.io/) automatically.
 
 ## Usage
 
-Save a list of websites (one per line) in a plain text file. Included in [2018-09-15-alexa-top-sites-10.txt](2018-09-15-alexa-top-sites-10.txt) is a list of 10 top sites from Alexa (Sep 2018).  
+Save a list of websites (one per line) in a plain text file. Included in [2018-09-15-alexa-top-sites-10.txt](2018-09-15-alexa-top-sites-10.txt) is a sample list of 10 top sites from Alexa (Sep 2018).  
 Make sure the script is executable by your user:
 ```sh
 chmod u+x from_list.py
 ```
 You may now run it:
 ```sh
-chromedriver &  # Implies --remote-debugging-port=9515. Runs in background.
+chromedriver 2> /dev/null &  # Implies --remote-debugging-port=9515. Runs in background.
 ./from_list.py 2018-09-15-alexa-top-sites-10.txt
 ```
 > See the file docstring in from_list.py for further info.
+
+Don't forget to stop chromedriver after running the Python script e.g.:
+```sh
+fg  # To bering chromedriver tot he background
+^C  # Ctrl + C
+```
--- from_list.py
+++ from_list.py
@@ -1,49 +1,41 @@
 #!/usr/bin/env python
 """
 Uses Selenium and Headless Chrome to scrap a list of websites.
-Each website is crawled and all pages are weighted (their full size is measured).
-Full size = all resources that have to be downloaded except cached content from previous pages in the same site.
+Each web site's home is loaded and fully weighted.
+Full weight = all resources that are downloaded initially by the browser to display the home page (no cache).
 
-Tested with Python 3.
-Required packages listed in requirements.txt
+Developed and tested with Python 3.6
+Requires Chrome and chromedriver
+Requires packages listed in requirements.txt
 
 Usage:
-    chromedriver
+    chromedriver 2> /dev/null &
     python -m from_list 2018-09-15-alexa-top-sites-10.txt
 
 where `site-list` is the path to a plain text file containing a list of websites, one per line.
 """
 import re
+import sys
+import time
 
 from argparse import ArgumentParser
 
 from selenium import webdriver
-
+from selenium.common.exceptions import WebDriverException, TimeoutException
 
 # 0. Loads list of web site addresses from file.
 parser = ArgumentParser()
 parser.add_argument('fname', help="Path to plain text file containing the list of websites, one per line")
 args = parser.parse_args()
 
-# # From https://stackoverflow.com/a/5494315/761963
-# re_3986_enhanced = re.compile(r"""
-#         # Parse and capture RFC-3986 Generic URI components. See https://tools.ietf.org/html/rfc3986#appendix-B
-#         ^
-#         (?:  (?P<scheme>    [^:/?#\s]+):// )?   # Optional scheme
-#         (?:(?P<authority>  [^/?#\s]*)  )?       # Optional authority
-#              (?P<path>        [^?#\s]*)         # Required path
-#         (?:\?(?P<query>        [^#\s]*)  )?     # Optional query
-#         (?:\#(?P<fragment>      [^\s]*)  )?     # Optional fragment
-#         $
-#         """, re.MULTILINE | re.VERBOSE)
-
 with open(args.fname) as f:
     # Skips empty lines and lines starting with `#` (comments).
-    # site_list = [re_3986_enhanced.match(l.strip())[2] for l in f if l.strip() and '#' != l.strip()[0]]
     site_list = [l.strip() for l in f if l.strip() and '#' != l.strip()[0]]
 
+    print(f'Loaded list of {len(site_list)} URLs:');
 
-# 1. Inits Selenium w/ Headless Chrome.
+
+# 1. Configs Selenium w/ Headless Chrome.
 
 # > See https://intoli.com/blog/running-selenium-with-headless-chrome/
 options = webdriver.ChromeOptions()
@@ -54,46 +46,55 @@ options.add_argument('headless')
 # and https://docs.seleniumhq.org/docs/04_webdriver_advanced.jsp#remotewebdriver
 capbs = webdriver.DesiredCapabilities.CHROME.copy()
 capbs.update({'loggingPrefs': {'performance': 'ALL'}, 'detach': False})
-driver = webdriver.Remote("http://127.0.0.1:9515", capbs, options=options)
-# ^ Requires chromedriver (server) running locally (on default port).
-
-# TODO: driver.set_page_load_timeout(10)  # Sets the amount of time to wait for a page load to complete before throwing an error
-# OR: driver.implicitly_wait(15)  # Sets a sticky timeout to implicitly wait to find element or complete command.
-
-# Enables DevTools Network panel.  # Unnecessary given loggingPrefs above.
-# driver.execute_cdp_cmd('Network.enable', {})
 
 
-# TODO: 2. Crawls each website.
+# 2. Crawls each website.
 tot_enc_data_len = {}
+tot_elapsed = 0
 for url in site_list:
-    print(f'Loading {url}...')
+    # Inits the WebDriver (again).
+    driver = webdriver.Remote("http://127.0.0.1:9515", capbs, options=options)
+    # ^ Requires chromedriver (server) running locally (on default port).
+    driver.set_page_load_timeout(10)  # Set the amount of time to wait for a page load to complete...
+    driver.set_script_timeout(10)  # Set the amount of time that the to wait during an execute_async_script call...
 
-    driver.get(url)
+    print(f'Loading {url}... ', end='');
+    sys.stdout.flush()
+
+    start = time.time()
+    try:
+        driver.get(url)
+    except TimeoutException as timeout:
+        print('Taking too long! Skipping after 10s waits');
+        continue
+    except WebDriverException as invalidurl:
+        print(f'Skipping invalid URL {url}');
+        # FIXME: Assumes it's an invalidurl error.
+        continue
+
+    end = time.time()
+    elapsed = end - start
+    tot_elapsed += elapsed
+
     logs = driver.execute('getLog', {'type': 'performance'})['value']
 
 
-    # TODO: 3. Calculate full size of each - from Network.loadingFinished INFO logs.
-    loading_finished = [l['message'] for l in logs if 'INFO' == l['level'] and 'Network.loadingFinished' in l['message']]
+    # 3. Calculate full size of each - from Network.loadingFinished INFO logs.
+    loading_finished = [l['message'] for l in logs if
+                        'INFO' == l['level'] and 'Network.loadingFinished' in l['message']]
     re_encdatalen = re.compile(r'^.*encodedDataLength":(-?[0-9]+),.*$')
     enc_data_len = [int(re_encdatalen.match(m)[1]) for m in loading_finished]
     tot_enc_data_len[url] = sum(enc_data_len)
 
-    # Answer stackoverflow.com/questions/51581676
-    # See stackoverflow.com/a/1611406/761963
-    # and stackoverflow.com/a/39321151/761963 ):
+    print(f'{tot_enc_data_len[url]}B, {elapsed:.3}s')
 
-    # Restarts driver to clear cache.
+    # Quits the driver to clear cache.
     driver.quit()
-    driver = webdriver.Remote("http://127.0.0.1:9515", capbs, options=options)
     # FIXME: Can we use driver.start_session(capabilities) with previous driver's capabilities?
-    # TODO: driver.implicitly_wait(15)  # Sets a sticky timeout to implicitly wait to find element or complete command.
 
 
-# TODO: 4. Calculates average.
-avg_enc_data_len = sum(tot_enc_data_len.values()) / len(tot_enc_data_len) / 10**6  # MB
+# 4. Calculates average.
+avg_enc_data_len = sum(tot_enc_data_len.values()) / len(tot_enc_data_len) / 10 ** 6  # MB
 # avg_enc_data_len = sum(tot_enc_data_len.values()) / len(tot_enc_data_len) / 2**20  # MiB
 
-print(f'The average web page size is {avg_enc_data_len} MB')
-
-driver.quit()
+print(f'The average web page size is {avg_enc_data_len}MB from {len(tot_enc_data_len)} processed web sites, loaded in {tot_elapsed:.3}s.')
