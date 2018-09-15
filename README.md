e6a5f697e71109fab26263d5431dcbde4927a5df
Jorge Orpinel <jorge@orpinel.com>
2018-09-15 07:09:29 -0500

    First working version (uses Network.loadingFinished)

--- README.md
+++ README.md
@@ -1,33 +1,42 @@
+© 2018 Dev Spotlight, All Rights Reserved  
+Author: Jorge Orpinel Perez @jorgeorpinel
+
 # Website Page Size Scraper
 
 Python script that uses Selenium and Headless Chrome to determine the average page size among a list of websites.
 
 ## Installation
 
-This tool uses Python 3.
+This tool was developed and ran with Python 3.6.5 on macOS 10.13
 
-### External dependency
+### External dependencies
 
-- Requres [ChromeDriver](https://sites.google.com/a/chromium.org/chromedriver/downloads) (`chromedriver` exec) in the system path - 2.41 used at the time of writing
+- [Chrome](https://www.google.com/chrome/) 68 installed
+- [ChromeDriver](https://sites.google.com/a/chromium.org/chromedriver) in the OS path - chromedriver 2.42 used
 
 ### Required Python package
 > See requirements.txt
 
-- Python language bindings for **Selenium WebDriver** (selenium==3.14 used at the time of writing)
+- Python language bindings for **Selenium WebDriver** – [selenium](https://seleniumhq.github.io/selenium/docs/api/py/api.html) 3.14 used
 
-Having [pip](https://pip.pypa.io/), install it with
+To install, we will use [virtualenv](https://virtualenv.pypa.io/en/stable/):
 ```sh
+virtualenv venv
+source venv/bin/activate
 pip install -r requirements.txt
 ```
+> Virtualenv installs [pip](https://pip.pypa.io/)
 
 ## Usage
 
-Save a list of websites (one per line) in a plain text file e.g. site-list and make sure the script is executable by you:
+Save a list of websites (one per line) in a plain text file. Included in [2018-09-15-alexa-top-sites-10.txt](2018-09-15-alexa-top-sites-10.txt) is a list of 10 top sites from Alexa (Sep 2018).  
+Make sure the script is executable by your user:
 ```sh
 chmod u+x from_list.py
 ```
 You may now run it:
 ```sh
-./from_list.py site-list
+chromedriver &  # Implies --remote-debugging-port=9515. Runs in background.
+./from_list.py 2018-09-15-alexa-top-sites-10.txt
 ```
 > See the file docstring in from_list.py for further info.
--- from_list.py
+++ from_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-
 """
 Uses Selenium and Headless Chrome to scrap a list of websites.
 Each website is crawled and all pages are weighted (their full size is measured).
@@ -9,36 +8,92 @@ Tested with Python 3.
 Required packages listed in requirements.txt
 
 Usage:
-    python -m from_list site-list
+    chromedriver
+    python -m from_list 2018-09-15-alexa-top-sites-10.txt
 
 where `site-list` is the path to a plain text file containing a list of websites, one per line.
 """
+import re
 
 from argparse import ArgumentParser
 
 from selenium import webdriver
 
 
-# 0. Loads list of sites from file.
+# 0. Loads list of web site addresses from file.
 parser = ArgumentParser()
 parser.add_argument('fname', help="Path to plain text file containing the list of websites, one per line")
 args = parser.parse_args()
 
+# # From https://stackoverflow.com/a/5494315/761963
+# re_3986_enhanced = re.compile(r"""
+#         # Parse and capture RFC-3986 Generic URI components. See https://tools.ietf.org/html/rfc3986#appendix-B
+#         ^
+#         (?:  (?P<scheme>    [^:/?#\s]+):// )?   # Optional scheme
+#         (?:(?P<authority>  [^/?#\s]*)  )?       # Optional authority
+#              (?P<path>        [^?#\s]*)         # Required path
+#         (?:\?(?P<query>        [^#\s]*)  )?     # Optional query
+#         (?:\#(?P<fragment>      [^\s]*)  )?     # Optional fragment
+#         $
+#         """, re.MULTILINE | re.VERBOSE)
+
 with open(args.fname) as f:
-    site_list = [l.strip() for l in f]
+    # Skips empty lines and lines starting with `#` (comments).
+    # site_list = [re_3986_enhanced.match(l.strip())[2] for l in f if l.strip() and '#' != l.strip()[0]]
+    site_list = [l.strip() for l in f if l.strip() and '#' != l.strip()[0]]
+
 
 # 1. Inits Selenium w/ Headless Chrome.
-# > See https://intoli.com/blog/running-selenium-with-headless-chrome/
 
+# > See https://intoli.com/blog/running-selenium-with-headless-chrome/
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
+# driver = webdriver.Chrome(chrome_options=options)
 
-driver = webdriver.Chrome(chrome_options=options)
+# See https://sites.google.com/a/chromium.org/chromedriver/logging/performance-log
+# and https://docs.seleniumhq.org/docs/04_webdriver_advanced.jsp#remotewebdriver
+capbs = webdriver.DesiredCapabilities.CHROME.copy()
+capbs.update({'loggingPrefs': {'performance': 'ALL'}, 'detach': False})
+driver = webdriver.Remote("http://127.0.0.1:9515", capbs, options=options)
+# ^ Requires chromedriver (server) running locally (on default port).
+
+# TODO: driver.set_page_load_timeout(10)  # Sets the amount of time to wait for a page load to complete before throwing an error
+# OR: driver.implicitly_wait(15)  # Sets a sticky timeout to implicitly wait to find element or complete command.
+
+# Enables DevTools Network panel.  # Unnecessary given loggingPrefs above.
+# driver.execute_cdp_cmd('Network.enable', {})
+
 
 # TODO: 2. Crawls each website.
+tot_enc_data_len = {}
+for url in site_list:
+    print(f'Loading {url}...')
 
-# TODO: 3. Calculate full size of each page.
+    driver.get(url)
+    logs = driver.execute('getLog', {'type': 'performance'})['value']
 
-# TODO: 4. Calculates averages.
+
+    # TODO: 3. Calculate full size of each - from Network.loadingFinished INFO logs.
+    loading_finished = [l['message'] for l in logs if 'INFO' == l['level'] and 'Network.loadingFinished' in l['message']]
+    re_encdatalen = re.compile(r'^.*encodedDataLength":(-?[0-9]+),.*$')
+    enc_data_len = [int(re_encdatalen.match(m)[1]) for m in loading_finished]
+    tot_enc_data_len[url] = sum(enc_data_len)
+
+    # Answer stackoverflow.com/questions/51581676
+    # See stackoverflow.com/a/1611406/761963
+    # and stackoverflow.com/a/39321151/761963 ):
+
+    # Restarts driver to clear cache.
+    driver.quit()
+    driver = webdriver.Remote("http://127.0.0.1:9515", capbs, options=options)
+    # FIXME: Can we use driver.start_session(capabilities) with previous driver's capabilities?
+    # TODO: driver.implicitly_wait(15)  # Sets a sticky timeout to implicitly wait to find element or complete command.
+
+
+# TODO: 4. Calculates average.
+avg_enc_data_len = sum(tot_enc_data_len.values()) / len(tot_enc_data_len) / 10**6  # MB
+# avg_enc_data_len = sum(tot_enc_data_len.values()) / len(tot_enc_data_len) / 2**20  # MiB
+
+print(f'The average web page size is {avg_enc_data_len} MB')
 
 driver.quit()
